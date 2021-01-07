4cf4a2053f7800bd3cbe9854579d7a00c4564f4f
Mohit Harge <hmohit05683@gmail.com>
2021-01-07 14:48:22 +0530

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    //global state variable
+    GlobleCounter: ''
+  },
+  getters: {
+    getCounterValue(state){
+      return state.GlobleCounter
+
+    }
   },
   mutations: {
+    ADD_COUNTER(state,data){
+      state.GlobleCounter=data
+
+    },
+    // REMOVE_COUNTER(state,data){
+
+    // }
+  
+
   },
   actions: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" height="" src="../assets/logo.png"><br><br>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div>
+        <b-button variant="primary"  @click="store()">Click Here</b-button><br><br>
+        <h1>Count : {{counter}} </h1><br>
+        <h1>Count from store : {{getCounterValueFromStore}} </h1><br>
+        <b-button variant="info"  @click="ResetCount()">Reset Count</b-button><br>
+    </div>
+      
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+  data(){
+    return{
+      counter: 0
+    }
+  },
+  computed:{
+    getCounterValueFromStore(){
+      return this.$store.getters['getCounterValue'];
+    }
+  },
+  created(){
+    this.counter = this.$store.state.GlobleCounter
+  },
+  methods:{
+    store(){
+      this.counter++
+      this.$store.commit('ADD_COUNTER',this.counter)
+    },
+  ResetCount(){
+    this.counter = 0
+    // this.$store.commit('ADD_COUNTER',this.counter)
+  },
+  },
+  
+};
 </script>
